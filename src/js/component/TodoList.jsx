74f9041a66a6b7e0d00c4fd03190c11095f48d47
pmart1n <123256191+pmart1n@users.noreply.github.com>
2024-02-07 00:09:48 +0000

    proyecto terminado

---
+++ src/js/component/TodoList.jsx
@@ -0,0 +1,41 @@
+import { element, string } from "prop-types";
+import React, { useState} from "react";
+export const TodoList = () => {
+    const [task, setTask] = useState('')
+    const [list, setList] = useState([])
+    const  addTask = (Event) => {
+        Event.preventDefault();
+        if (task.trim() !== '') { // Verifica que el valor no esté vacío
+            setList([...list, task.trim()]);
+            setTask('');
+        }
+    }
+    const deleteTask = (item) => {
+        setList(list.filter((element) => element != item))
+    }
+    return (
+    <div>
+        <h1 className="text-success">Todo List </h1>
+        <div className="container col-5">
+        <form onSubmit={addTask}>
+            <input type="text" className="form-control my-5" placeholder={list.length ? "Anadir tareas" : "No hay tareas pendientes, añada una.."}
+            value={task}
+            onChange={(Event) => setTask(Event.target.value)}
+            />
+        </form>
+            <h2 className="my-5 text-danger">Tareas pendientes</h2>
+            <div>
+                <ul className="list-group">
+                    {list.map((item, id) => <li key={id} className="list-group-item d-flex justify-content-between hidden-icon" >
+                        {item}
+                        <span onClick={() => deleteTask(item)}>
+                        <i className="fas fa-trash text-danger"></i>
+                        </span>
+                    </li>)
+                    }
+                </ul>
+            </div>
+        </div>
+    </div>
+    )
+}
